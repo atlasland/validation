0f5ffafa55f10fc6ebc1a049bde3ff70e3c5cb20
Gabriel Izaias <gabriel.izaias@gmail.com>
2021-02-08 12:08:23 +0100

    add email validator

---
+++ email.ts
@@ -0,0 +1,12 @@
+/**
+ * Validates an email according to RFC5322 (or as close as possible)
+ * @param value The email string to be validated
+ * @see https://www.regular-expressions.info/email.html
+ * @see https://tools.ietf.org/html/rfc5322#section-3.4
+ * @see https://stackoverflow.com/a/201378/1001673
+ */
+export function email(value: string): boolean {
+	return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+		value
+	)
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,16 @@
 
 This is a set of validation functions that I often find myself using on projects.
 
+## Validations
+
+### Email
+
+Validates an email according to [RFC5322](https://tools.ietf.org/html/rfc5322#section-3.4) (or as close as possible).
+
+```js
+import { email } from "@gabrielizaias/validation/email.js";
+
+if (email('test@example.com')) {
+	console.log('valid')
+}
+```
