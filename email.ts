c0da8f8f8aa7706a3052863e9cd629953bd7c775
Gabriel Silva <gabriel.izaias@gmail.com>
2022-07-08 19:45:16 -0300

    feat(email): expose validation regex

--- email.ts
+++ email.ts
@@ -1,3 +1,6 @@
+export const REGEX =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
 /**
  * Validates an email according to RFC5322 (or as close as possible).
  *
@@ -7,8 +10,5 @@
  * @see https://stackoverflow.com/a/201378/1001673
  */
 export function valid(value: string): boolean {
-  return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    .test(
-      value,
-    );
+  return REGEX.test(value);
 }
